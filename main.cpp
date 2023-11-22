f2c4b4fc515e0cc60df7cae5d68048df1190e8a9
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-11-22 21:42:40 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,56 +3,77 @@
 
 using namespace std;
 
+const double TWO = 2.0;
+const double THREE = 3.0;
+
 double f(double x) {
-    /*
-        Pre: real number x
-        Post: mathematical function f(x)
-    */
-    return sin(x)*x;
+    // Pre: real number x
+    // Post: mathematical function f(x)
+    return sin(x) * x;
 }
 
 double simpsonIntegral(double a, double b, int n) {
-    /*
-        Pre: limits a,b and real number n (the number of parts that is divided the interval [a,b])
-        Post: aproximation of the integral of function f(x) in interval [a,b] with the Simpson method
-    */
+    // Pre: limits a, b and real number n (the number of parts that divide the interval [a, b])
+    // Post: approximation of the integral of function f(x) in the interval [a, b] with the Simpson method
 
-    double Part = a, E = f(a) + f(b), S = 0, P = 0, h = (b-a)/n;
+    double h = (b - a) / n;
+    double result = f(a) + f(b);
 
-    for(int i = 1; i <= n-1; i++) {
-        Part += h;
-        if(i % 2 != 0) {
-            S += f(Part);
-        } else {
-            P += f(Part);
-        }
+    for (int i = 1; i <= n - 1; i++) {
+        double x = a + i * h;
+        result += (i % 2 == 0) ? TWO * f(x) : THREE * f(x);
     }
 
-    return (h/3)*(E + 4*S + 2*P);
-
+    return (h / THREE) * result;
 }
 
 double trapezoidIntegral(double a, double b, int n) {
-    /*
-        Pre: limits a,b and real number n (the number of parts that is divided the interval [a,b])
-        Post: aproximation of the integral of function f(x) in interval [a,b] with the Trapezoid method
-    */
+    // Pre: limits a, b and real number n (the number of parts that divide the interval [a, b])
+    // Post: approximation of the integral of function f(x) in the interval [a, b] with the Trapezoid method
 
-    double Part = a, E = f(a) + f(b), P = 0, h = (b-a)/n;
+    double h = (b - a) / n;
+    double result = f(a) + f(b);
 
-    for(int i = 1; i <= n-1; i++) {
-        Part += h;
-        P += f(Part);
+    for (int i = 1; i <= n - 1; i++) {
+        double x = a + i * h;
+        result += TWO * f(x);
     }
 
-    return (h/2)*(E + 2*P);
-
+    return (h / TWO) * result;
 }
 
 int main() {
-    // examples of use
-    double resultSimpson = simpsonIntegral(0,2,10);
-    double resultTrapezoid = trapezoidIntegral(0,2,10);
-    cout << resultSimpson << endl << resultTrapezoid;
+    int choice;
+    double a, b;
+    int n;
+
+    cout << "Enter the limits of integration [a, b]: ";
+    cin >> a >> b;
+
+    cout << "Enter the number of subintervals (n): ";
+    cin >> n;
+
+    cout << "Select the integration method:" << endl;
+    cout << "1. Simpson's Method" << endl;
+    cout << "2. Trapezoid Method" << endl;
+    cout << "Enter your choice (1 or 2): ";
+    cin >> choice;
+
+    double result;
+
+    switch (choice) {
+        case 1:
+            result = simpsonIntegral(a, b, n);
+            cout << "Simpson Integral: " << result << endl;
+            break;
+        case 2:
+            result = trapezoidIntegral(a, b, n);
+            cout << "Trapezoid Integral: " << result << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please enter 1 or 2." << endl;
+            return 1; // Return an error code
+    }
+
     return 0;
 }
